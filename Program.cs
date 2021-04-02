1f515ecbbfdeec2c71b3c1f1e784f730c83644da
ADVallespir <advallespir@gmail.com>
2021-04-02 14:27:36 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace guia_1_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa un texto para convertir en minusculas");
+            string texto = Console.ReadLine();
+            string textoToLower = texto.ToLower(new System.Globalization.CultureInfo("es-ES", false));
+            Console.WriteLine(textoToLower);
+            Console.ReadLine();
+
+        }
+    }
+}
